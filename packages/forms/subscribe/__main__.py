2e81aa9901dde218bb9d01b7f7e2c85f68c043b1
0x213F <josh@schultheiss.io>
2022-10-22 13:00:22 -0700

    Refactor to create beginnings of subcribe form

--- packages/forms/subscribe/__main__.py
+++ packages/forms/subscribe/__main__.py
@@ -1,6 +1,7 @@
+import boto3
+
 def main(args):
-      name = args.get("name", "stranger")
+      name = args.get("name", "Sounds Radiant")
       greeting = "Hello " + name + "!"
       print(greeting)
       return {"body": greeting}
-  
