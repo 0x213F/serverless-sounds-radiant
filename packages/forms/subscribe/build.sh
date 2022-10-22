967f0576833c981082a3d444e596d8421650e70e
0x213F <josh@schultheiss.io>
2022-10-22 14:28:23 -0700

    Debug subscribe 2

--- packages/forms/subscribe/build.sh
+++ packages/forms/subscribe/build.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
+echo "hello 1"
 set -e
+
+echo "hello 2"
 virtualenv --without-pip virtualenv
+echo "hello 3"
 pip install -r requirements.txt --target virtualenv/lib/python3.9/site-packages
